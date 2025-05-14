77a73d1377829bcf7b7990e4c4ecde0e26de47a9
Alexisfllv <alexis-fl-live@hotmail.com>
2025-05-13 23:57:59 -0500

    Exception

--- src/main/java/edu/com/javaesencial07salesapi/exception/GlobalErrorHandler.java
+++ src/main/java/edu/com/javaesencial07salesapi/exception/GlobalErrorHandler.java
@@ -14,23 +14,23 @@ import java.time.LocalDateTime;
 @RestControllerAdvice
 public class GlobalErrorHandler {
 
-//    @ExceptionHandler(ModelNotFoundException.class)
-//    public ResponseEntity<CustomErrorResponse> handleModelNotFoundException(ModelNotFoundException ex , WebRequest req){
-//        CustomErrorResponse error = new CustomErrorResponse
-//                (LocalDateTime.now(), ex.getMessage(),req.getDescription(false));
-//        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-//    }
-
-
-    // DESDE SPRING BOOT 3 + ProblemDetail
     @ExceptionHandler(ModelNotFoundException.class)
-    public ProblemDetail handleModelNotFoundException(ModelNotFoundException ex , WebRequest req){
-        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
-        pd.setTitle("Model not found ");
-        pd.setType(URI.create(req.getContextPath()));
-        pd.setProperty("path", req.getDescription(false));
-        pd.setProperty("timestamp", LocalDateTime.now());
-        pd.setProperty("message", ex.getMessage());
-        return pd;
+    public ResponseEntity<CustomErrorResponse> handleModelNotFoundException(ModelNotFoundException ex , WebRequest req){
+        CustomErrorResponse error = new CustomErrorResponse
+                (LocalDateTime.now(), ex.getMessage(),req.getDescription(false));
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
+
+
+//    // DESDE SPRING BOOT 3 + ProblemDetail
+//    @ExceptionHandler(ModelNotFoundException.class)
+//    public ProblemDetail handleModelNotFoundException(ModelNotFoundException ex , WebRequest req){
+//        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+//        pd.setTitle("Model not found ");
+//        pd.setType(URI.create(req.getContextPath()));
+//        pd.setProperty("path", req.getDescription(false));
+//        pd.setProperty("timestamp", LocalDateTime.now());
+//        pd.setProperty("message", ex.getMessage());
+//        return pd;
+//    }
 }

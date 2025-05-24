6383549111676327487c74a6696d6ab904667251
Alexisfllv <alexis-fl-live@hotmail.com>
2025-05-24 10:47:33 -0500

    feat (service impl) : aplicando java reflection para update id

--- src/main/java/edu/com/javaesencial07salesapi/service/implementation/CRUDIMPL.java
+++ src/main/java/edu/com/javaesencial07salesapi/service/implementation/CRUDIMPL.java
@@ -4,6 +4,8 @@ import edu.com.javaesencial07salesapi.exception.ModelNotFoundException;
 import edu.com.javaesencial07salesapi.repo.GenericRepo;
 import edu.com.javaesencial07salesapi.service.GenericService;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 
 public abstract class CRUDIMPL<T,ID> implements GenericService<T,ID> {
@@ -30,8 +32,20 @@ public abstract class CRUDIMPL<T,ID> implements GenericService<T,ID> {
 
     @Override
     public T update(T t, ID id) {
-        return getRepo().save(t);
+        try {
+            Class<?> clazz = t.getClass();
+            String methodName = "setId" + clazz.getSimpleName();
+            Method setIdMethod = clazz.getMethod(methodName, id.getClass());
+            setIdMethod.invoke(t, id);
+
+            getRepo().findById(id)
+                    .orElseThrow(() -> new ModelNotFoundException("ITEM NO ENCONTRADO: " + id));
+            return getRepo().save(t);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException("Error al usar reflexión en update()", e);
+        }
     }
+
 
     @Override
     public void deleteById(ID id) {
